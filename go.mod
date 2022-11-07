8170d42a42ce70f7e064d3c8d611a0d1f6719c1b
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-11-06 22:11:18 -0500

    feat: endpoints declared

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/concurrente-parcial/back
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,40 +1,20 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"log"
 	"net/http"
-	"os"
-	"time"
+
+	"github.com/gorilla/mux"
 )
 
-const serverPort = 3333
+func initializeRouter() {
 
-func main() {
-	go func() {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Printf("server: %s /\n", r.Method)
-		})
-		server := http.Server{
-			Addr:    fmt.Sprintf(":%d", serverPort),
-			Handler: mux,
-		}
-		if err := server.ListenAndServe(); err != nil {
-			if !errors.Is(err, http.ErrServerClosed) {
-				fmt.Printf("error running http server: %s\n", err)
-			}
-		}
-	}()
+	r := mux.NewRouter()
 
-	time.Sleep(100 * time.Millisecond)
-	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
-	res, err := http.Get(requestURL)
-	if err != nil {
-		fmt.Printf("error making http request: %s\n", err)
-		os.Exit(1)
-	}
+	r.HandleFunc("/login", Login).Methods("POST")
 
-	fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	r.HandleFunc("/verification", Verificatio).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":9000", r))
+
 }
